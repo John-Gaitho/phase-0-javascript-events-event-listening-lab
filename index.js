34ef8476bda157692b0e39b88298026823b4b6f5
John Gaitho <jgaitho016@gmail.com>
2024-10-08 12:43:48 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
-}
+
+    function addingEventListener() {
+        const input = document.getElementById('button');
+        input.addEventListener('click', function() {
+        alert('Hee hee, that Tickles!!');
+      })
+      }
+      
+      addingEventListener();
+      
